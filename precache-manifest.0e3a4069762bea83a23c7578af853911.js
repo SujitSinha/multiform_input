51a18975d210150a951c9a31854fa0caabffdd14
su7jit <sujitsinha.yo@gmail.com>
2020-08-17 14:47:37 +0530

    Updates

---
+++ precache-manifest.0e3a4069762bea83a23c7578af853911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f50f7db3fb01c98b1489d36688909a6",
+    "url": "/multiform_input/index.html"
+  },
+  {
+    "revision": "ba3731fe8f3a7922cf1e",
+    "url": "/multiform_input/static/css/main.6332ab80.chunk.css"
+  },
+  {
+    "revision": "99924c020e3bbce5f4a0",
+    "url": "/multiform_input/static/js/2.2831e379.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/multiform_input/static/js/2.2831e379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba3731fe8f3a7922cf1e",
+    "url": "/multiform_input/static/js/main.89e748fa.chunk.js"
+  },
+  {
+    "revision": "20e5cf0b05e0df06c79a",
+    "url": "/multiform_input/static/js/runtime-main.c2c02df0.js"
+  }
+]);
